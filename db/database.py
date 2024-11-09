5ff5091e40dadf483588440031971ae00a5806b7
Nicolas <nicopuegher@gmail.com>
2024-11-09 11:12:57 -0300

    Update database URL to use local variables in /db/database.py

--- db/database.py
+++ db/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///dna_sequences.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
