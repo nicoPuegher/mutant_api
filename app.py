28a0db5de91658847738ea41bb7f122f521ac8cb
Nicolas <nicopuegher@gmail.com>
2024-11-09 10:56:10 -0300

    Add new records to the database in /mutant/ route in /app.py

--- app.py
+++ app.py
@@ -17,6 +17,21 @@ def mutant():
     if not dna or not isinstance(dna, list) or any(len(row) != len(dna) for row in dna):
         return jsonify({"error": "Invalid DNA format"}), 400
 
+    is_dna_mutant = is_mutant(dna)
+    sequence_str = "".join(dna)
+
+    db = next(get_db())
+    existing_dna = (
+        db.query(DNASequence).filter(DNASequence.sequence == sequence_str).first()
+    )
+
+    if not existing_dna:
+        new_dna_record = DNASequence(
+            sequence=sequence_str, is_mutant=int(is_dna_mutant)
+        )
+        db.add(new_dna_record)
+        db.commit()
+
     if is_mutant(dna):
         return jsonify({"message": "Mutant detected"}), 200
     else:
